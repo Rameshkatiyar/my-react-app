c15d1649be2b5d97ccb2464e72cfc7dc13c9af92
Rameshkatiyar <er.rameshkatiyar@gmail.com>
2022-09-27 23:35:05 +0530

    Used the API to use the array-map

--- src/components/ArrayMap.js
+++ src/components/ArrayMap.js
@@ -5,35 +5,36 @@ class ArrayMap extends Component {
     constructor() {
         super();
         this.state = {
-            users: [
-                {
-                    id: 1,
-                    name: 'Ramesh'
-                },
-                {
-                    id: 2,
-                    name: 'Shiwali'
-                },
-                {
-                    id: 3,
-                    name: 'Dilip'
-                },
-                {
-                    id: 4,
-                    name: 'Kiran'
-                }
-            ]
+            users: []
         }
+        console.log('Step 1')
+    }
+
+    componentDidMount() {
+        console.log('Step 3')
+
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((resp) => resp.json())
+        .then(
+            (usersData) => this.setState(
+                () => {return {users: usersData}},
+                () => console.log(this.state)
+            )
+        )
+        .catch(
+            () => console.log('No data from API.')
+        );
     }
 
     render() {
+        console.log('Step 2')
         return (
             <div>
                 <h1>Array Map:</h1>
                 {
                     this.state.users.map(
                         (user) => {
-                            return <h2 key={user.id}>{user.name}</h2>
+                            return <h2 key={user.id}>{user.id}. {user.name}</h2>
                         }
                     )
                 }
